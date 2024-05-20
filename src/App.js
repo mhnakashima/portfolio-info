c6f4a154860283d41b937f5aa25db25d0ee51ad9
Michael Nakashima <mhnakashima@gmail.com>
2024-05-20 17:10:29 -0300

    adding tailwind

--- src/App.js
+++ src/App.js
@@ -1,9 +1,26 @@
 import React from 'react';
+import "./styles.scss";
+import logo from './assets/images/profile.webp';
 
-const App =  () => {
+const App = () => {
     return (
-        <aside>
-            Menu info
+        <aside id="app-menu-info" className={`app-menu-info`}>
+            <div className='section'>
+                <img src={logo} alt="My personal photo" />
+                <h1 className={`mt-1 text-2xl font-bold`}>Michael Nakashima | Naka</h1>
+                <h2 className='mb-3'>Front-end Enginner</h2>
+
+                <p className={`mb-3 text font-light `}>
+                    Front end Engineer with 15 years of experience, used to work with small and bigger teams,
+                    developing solutions like apps, interfaces, websites, helping colleagues to developer themselves
+                    helping them with Front end technical knowledge, proven experience developing solutions
+                    for companies and clients.
+                </p>
+            </div>
+
+            <div className='section'>
+                <h3 className='font-bold'>Contact Details</h3>
+            </div>
         </aside>
     );
 };
