d9c7af3d9e74239e5c95b9b01ce8717dbb7a363c
Michael Nakashima <mhnakashima@gmail.com>
2024-05-21 12:23:03 -0300

    adding tests to portfolio info

---
+++ src/components/Badge/Badge.test.js
@@ -0,0 +1,63 @@
+import React from 'react';
+import { getByText, render, waitFor } from '@testing-library/react';
+import '@testing-library/jest-dom';
+import { Badge } from './Badge';  // Adjust the import path as necessary
+import Icon from '@mdi/react';
+import { mdiReload } from '@mdi/js';
+import Avatar from '../Avatar/Avatar';
+
+// Mocking external libraries
+jest.mock('@mdi/react', () => {
+    return {
+        __esModule: true,
+        default: (props) => <div data-testid="icon">{props.path}</div>,
+    };
+});
+
+jest.mock('../Avatar/Avatar', () => {
+    return {
+        __esModule: true,
+        default: ({ children }) => <div data-testid="avatar">{children}</div>,
+    };
+});
+
+jest.mock('@mdi/js', () => {
+    return {
+        __esModule: true,
+        mdiReload: 'mdiReloadMock',  // Mocking the mdiReload value
+    };
+});
+
+describe('Badge component', () => {
+    test('should render icon, label, and text', () => {
+        const iconPath = 'mdiTestIcon';
+        const label = 'Test Label';
+        const text = 'Test Text';
+
+        const { getByText, getByTestId } = render(<Badge icon={iconPath} label={label} text={text} />);
+
+        // Check if Avatar component renders
+        expect(getByTestId('avatar')).toBeInTheDocument();
+
+        // Check if Icon component renders with the correct path
+        expect(getByTestId('icon')).toHaveTextContent(iconPath);
+
+        // Check if label and text are rendered
+        expect(getByText(label)).toBeInTheDocument();
+        expect(getByText(text)).toBeInTheDocument();
+    });
+
+    test('should render default values when no props are provided', async () => {
+        const { container, getByTestId } = render(<Badge />);
+
+        // Check if Avatar component renders
+        expect(getByTestId('avatar')).toBeInTheDocument();
+
+        // Check if Icon component renders with the default mdiReload path
+        expect(getByTestId('icon')).toHaveTextContent('mdiReloadMock');
+
+        // Check if empty strings are rendered for label and text by class names
+        waitFor(() => expect(container.querySelector('.badge--label')).toHaveTextContent(''));
+        waitFor(() => expect(container.querySelector('.badge--text')).toHaveTextContent(''));
+    });
+});
