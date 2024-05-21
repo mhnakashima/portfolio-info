f701a0a4d150c0e91597a38784d4c31b67d3f90c
Michael Nakashima <mhnakashima@gmail.com>
2024-05-21 15:12:39 -0300

    Stylizing and component items

--- src/App.js
+++ src/App.js
@@ -1,64 +1,78 @@
+import { mdiEmail, mdiGithub, mdiHelpCircleOutline, mdiLinkedin, mdiWebBox } from '@mdi/js';
 import React from 'react';
-import "./styles.scss";
 import logo from './assets/images/profile.webp';
-import Avatar from './components/Avatar/Avatar';
-import Icon from '@mdi/react';
-import { mdiEmail, mdiWebBox, mdiLinkedin, mdiGithub } from '@mdi/js';
 import { Badge } from './components/Badge/Badge';
+import "./styles.scss";
+import Icon from '@mdi/react';
+import { Header } from './components/Header/Header';
 
 const App = () => {
     return (
         <aside id="app-menu-info" className={`app-menu-info`}>
-            <div className='section'>
-                <img src={logo} alt="My personal photo" />
-                <h1 className={`mt-1 text-2xl font-bold`}>Michael Nakashima | Naka</h1>
-                <h2 className='mb-3'>Front-end Enginner</h2>
+            
+            <Header />
 
-                <p className={`mb-3 text font-light `}>
-                    Front end Engineer with 15 years of experience, used to work with small and bigger teams,
-                    developing solutions like apps, interfaces, websites, helping colleagues to developer themselves
-                    helping them with Front end technical knowledge, proven experience developing solutions
-                    for companies and clients.
-                </p>
+            <div className='section'>
+                <h3 className='font-bold'>Personal Details</h3>
+
+                <div className='mt-3 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4'>
+                    <div className='grid grid-cols-2 items-center mb-4'>
+                        <span className='text-sm font-light'>Work Exp.</span>
+                        <span className='text-base font-light'>15 years</span>
+                    </div>
+
+                    <div className='grid grid-cols-2 items-center mb-4'>
+                        <span className='text-sm font-light'>Education.</span>
+                        <span className='text-base font-light'>Pontificia Universidade Católica - PUC - MG</span>
+                    </div>
+
+                    <div className='grid grid-cols-2 items-center mb-4'>
+                        <span className='text-sm font-light'>Current<br />Occupation.</span>
+                        <span className='text-base font-light'>Software Engineer at Ciandt (2022)</span>
+                    </div>
+
+                    <div className='grid grid-cols-2 items-center mb-4'>
+                        <span className='text-sm font-light'>Languages.</span>
+                        <p className='text-base font-light'>
+                            <ul className='list-none'>
+                                <li>PT-BR: Native</li>
+                                <li>English: B2</li>
+                                <li>Spanish: B1</li>
+                            </ul>
+                        </p>
+                    </div>
+
+                    <div className='grid grid-cols-2 items-center mb-4'>
+                        <span className='text-sm font-light'>Date of Birth</span>
+                        <span className='text-base font-light'>11/13/1987</span>
+                    </div>
+                </div>
             </div>
 
             <div className='section'>
                 <h3 className='font-bold'>Contact Details</h3>
 
-                <div className='mt-3 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4'>
+                <div className='mt-3 grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4'>
                     <Badge icon={mdiEmail} label={`Email`} text={`mhnakashima@gmail.com`} />
-                    
-                    <div className='items flex'>
-                        <Avatar>
-                            <Icon path={mdiWebBox} size={`1rem`} />
-                        </Avatar>
-                        <div className='ml-3 description'>
-                            <span>Website</span>
-                            <h4 className='text-base font-bold'><a href='http://mhnakashima.com' target='_blank'>http://mhnakashima.com</a></h4>
-                        </div>
-                    </div>
-
-                    <div className='items flex'>
-                        <Avatar>
-                            <Icon path={mdiLinkedin} size={`1rem`} />
-                        </Avatar>
-                        <div className='ml-3 description'>
-                            <span>Linkedin</span>
-                            <h4 className='text-base font-bold'><a href='https://www.linkedin.com/in/mhnakashima/' target='_blank'>/mhnakashima</a></h4>
-                        </div>
-                    </div>
-
-                    <div className='items flex'>
-                        <Avatar>
-                            <Icon path={mdiGithub} size={`1rem`} />
-                        </Avatar>
-                        <div className='ml-3 description'>
-                            <span>Github</span>
-                            <h4 className='text-base font-bold'><a href='https://github.com/mhnakashima' target='_blank'>/Github</a></h4>
-                        </div>
-                    </div>
+                    <Badge icon={mdiWebBox} label={`Website`} text={<a href='http://mhnakashima.com' target='_blank'>http://mhnakashima.com</a>} />
+                    <Badge icon={mdiLinkedin} label={`Linkedin`} text={<a href='https://www.linkedin.com/in/mhnakashima/' target='_blank'>mhnakashima</a>} />
+                    <Badge icon={mdiGithub} label={`Github`} text={<a href='https://github.com/mhnakashima' target='_blank'>/Github</a>} />
                 </div>
             </div>
+
+            <div className='section'>
+                <h3 className='font-bold'>About the Containers</h3>
+
+                <p className='text text-base font-light'>
+                    Hello, HR, Tech or anyone who wants to know some info about this Portfolio that I have created. So,
+                    After many tech interviews I wanted to put everything that I am using to work on this website from
+                    every single step since the conception to delivery, in some parts of this site, you will able to see a <Icon className='inline' path={mdiHelpCircleOutline} size={`1rem`} aria-label='Interrogation icon' />&nbsp; 
+                    which will describe the tech stack info about it.<br /><br />I hope this information helps you to validate
+                    any information that you may need.
+                </p>
+
+            </div>
+
         </aside>
     );
 };
